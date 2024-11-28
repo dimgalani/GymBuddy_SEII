dd03c9b67b30b237887edad6a20b1e7d3319e718
Emily Palaska <aimilia.p2@gmail.com>
2024-11-28 11:50:21 +0200

    change wrong test name

--- test/init.test.js
+++ test/init.test.js
@@ -15,14 +15,14 @@ test.after.always((t) => {
 	t.context.server.close();
 });
 
-test("GET /user/{usename}/settings/goals with Bad Request (no day parameter)", async (t) => {
+test("GET /user/{usename}/progress/goals with Bad Request (no day parameter)", async (t) => {
 	const { body, statusCode } = await t.context.got("user/default/planner/progress/goals", {
 		throwHttpErrors: false // Prevent `got` from rejecting the promise on 400 responses
 	});
 	t.is(statusCode, 400);
 });
 
-test("GET /user/{usename}/settings/goals with Correct Request (Mock Data)", async (t) => {
+test("GET /user/{usename}/progress/goals with Correct Request (Mock Data)", async (t) => {
 	const { body, statusCode } = await t.context.got("user/john_doe/planner/progress/goals", {
 		searchParams: {
 			day: 2
