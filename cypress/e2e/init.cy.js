074f4c9f08b395895fca1935360f237b90f335e9
Emily Palaska <aimilia.p2@gmail.com>
2024-12-08 13:18:33 +0200

    make init cy and try test

--- cypress/e2e/init.cy.js
+++ cypress/e2e/init.cy.js
@@ -1,9 +1,24 @@
-describe("Test", () => {
+describe("Initialize tests", () => {
     beforeEach(() => {
         cy.visit("http://locahost:8080");
     })
 })
 
-it("Correct", () => {
-    cy.get("#username").type
-})
+describe('Dropdown Navigation Test - Double Click', () => {
+    it('should navigate to /default after clicking the dropdown with label "Default" twice', () => {
+      // Visit the localhost page
+      cy.visit('http://localhost:8080/docs');
+  
+      // Find the dropdown menu with label "Default"
+      const dropdown = cy.contains('label', 'Default')
+        .parent()
+        .find('select');
+  
+      // Click the dropdown twice
+      dropdown.select('Default'); // First click
+      dropdown.select('Default'); // Second click
+  
+      // Assert that the URL contains "/default"
+      cy.url().should('include', '/default');
+    });
+  });
