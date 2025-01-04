a7b1e5ede46d4aab6414658df4616128bbf27f02
Afroditi Avramidou <153501250+AfroditiAvr@users.noreply.github.com>
2025-01-04 15:11:56 +0200

    Update Default.js

--- controllers/Default.js
+++ controllers/Default.js
@@ -1,9 +1,23 @@
 'use strict';
+/* 
+ * This module defines the route handlers for various API endpoints. Each handler
+ * interacts with the corresponding service layer (DefaultService) and sends
+ * responses to clients using a utility writer module.
+ */
 
 var utils = require('../utils/writer.js');
 var Default = require('../service/DefaultService');
 
-module.exports.cancelReservation = function cancelReservation (req, res, next, username, day, time) {
+/*
+ * Cancels a reservation for a specific user. 
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ * @param {Function} next - The middleware next function.
+ * @param {string} username - The username of the user.
+ * @param {string} day - The day of the reservation.
+ * @param {string} time - The time of the reservation.
+ */
+module.exports.cancelReservation = function cancelReservation(req, res, next, username, day, time) {
   Default.cancelReservation(username, day, time)
     .then(function (response) {
       utils.writeJson(res, response, response.code);
@@ -13,18 +27,27 @@ module.exports.cancelReservation = function cancelReservation (req, res, next, u
     });
 };
 
-module.exports.checkGoalsFromInfo = function checkGoalsFromInfo (req, res, next, username, currentBodyWeight) {
+/*
+ * Checks goals from personal information for a specific user.
+ * @param {string} username - The username of the user.
+ * @param {number} currentBodyWeight - The current body weight of the user.
+ */
+module.exports.checkGoalsFromInfo = function checkGoalsFromInfo(req, res, next, username, currentBodyWeight) {
   Default.checkGoalsFromInfo(username, currentBodyWeight)
     .then(function (response) {
       utils.writeJson(res, response, response.code);
     })
     .catch(function (error) {
-      // console.log(error);
       utils.writeJson(res, error.message, error.code);
     });
 };
 
-module.exports.checkGoalsFromProgress = function checkGoalsFromProgress (req, res, next, username, day) {
+/*
+ * Checks goals based on progress data for a specific user.
+ * @param {string} username - The username of the user.
+ * @param {string} day - The day for which progress data is checked.
+ */
+module.exports.checkGoalsFromProgress = function checkGoalsFromProgress(req, res, next, username, day) {
   Default.checkGoalsFromProgress(username, day)
     .then(function (response) {
       utils.writeJson(res, response, response.code);
@@ -34,7 +57,12 @@ module.exports.checkGoalsFromProgress = function checkGoalsFromProgress (req, re
     });
 };
 
-module.exports.createCustomExercise = function createCustomExercise (req, res, next, body, username) {
+/*
+ * Creates a custom exercise for a specific user.
+ * @param {Object} body - The details of the custom exercise.
+ * @param {string} username - The username of the user.
+ */
+module.exports.createCustomExercise = function createCustomExercise(req, res, next, body, username) {
   Default.createCustomExercise(body, username)
     .then(function (response) {
       utils.writeJson(res, response, response.code);
@@ -44,7 +72,12 @@ module.exports.createCustomExercise = function createCustomExercise (req, res, n
     });
 };
 
-module.exports.getAvailableReservations = function getAvailableReservations (req, res, next, username, day) {
+/*
+ * Retrieves available reservations for a specific user.
+ * @param {string} username - The username of the user.
+ * @param {string} day - The day for which reservations are requested.
+ */
+module.exports.getAvailableReservations = function getAvailableReservations(req, res, next, username, day) {
   Default.getAvailableReservations(username, day)
     .then(function (response) {
       utils.writeJson(res, response, response.code);
@@ -54,7 +87,12 @@ module.exports.getAvailableReservations = function getAvailableReservations (req
     });
 };
 
-module.exports.getDayofPlanner = function getDayofPlanner (req, res, next, username, day) {
+/*
+ * Retrieves the planner data for a specific day.
+ * @param {string} username - The username of the user.
+ * @param {string} day - The day for which planner data is requested.
+ */
+module.exports.getDayofPlanner = function getDayofPlanner(req, res, next, username, day) {
   Default.getDayofPlanner(username, day)
     .then(function (response) {
       utils.writeJson(res, response, response.code);
@@ -64,7 +102,11 @@ module.exports.getDayofPlanner = function getDayofPlanner (req, res, next, usern
     });
 };
 
-module.exports.getDropDownMenuList = function getDropDownMenuList (req, res, next, username) {
+/*
+ * Retrieves the drop-down menu list for a specific user.
+ * @param {string} username - The username of the user.
+ */
+module.exports.getDropDownMenuList = function getDropDownMenuList(req, res, next, username) {
   Default.getDropDownMenuList(username)
     .then(function (response) {
       utils.writeJson(res, response, response.code);
@@ -74,7 +116,11 @@ module.exports.getDropDownMenuList = function getDropDownMenuList (req, res, nex
     });
 };
 
-module.exports.getExerciseCatalog = function getExerciseCatalog (req, res, next, username) {
+/*
+ * Retrieves the exercise catalog for a specific user.
+ * @param {string} username - The username of the user.
+ */
+module.exports.getExerciseCatalog = function getExerciseCatalog(req, res, next, username) {
   Default.getExerciseCatalog(username)
     .then(function (response) {
       utils.writeJson(res, response, response.code);
@@ -84,6 +130,8 @@ module.exports.getExerciseCatalog = function getExerciseCatalog (req, res, next,
     });
 };
 
+
+// Function to get exercise progress for a specific exercise
 module.exports.getExerciseProgress = function getExerciseProgress (req, res, next, username, exerciseName) {
   Default.getExerciseProgress(username, exerciseName)
     .then(function (response) {
@@ -94,6 +142,7 @@ module.exports.getExerciseProgress = function getExerciseProgress (req, res, nex
     });
 };
 
+// Function to get the user's reservations
 module.exports.getMyReservations = function getMyReservations (req, res, next, username) {
   Default.getMyReservations(username)
     .then(function (response) {
@@ -104,6 +153,7 @@ module.exports.getMyReservations = function getMyReservations (req, res, next, u
     });
 };
 
+// Function to get personal information for a user
 module.exports.getPersonalInfo = function getPersonalInfo (req, res, next, username) {
   Default.getPersonalInfo(username)
     .then(function (response) {
@@ -114,8 +164,8 @@ module.exports.getPersonalInfo = function getPersonalInfo (req, res, next, usern
     });
 };
 
+// Function to make a reservation for the user
 module.exports.makeReservation = function makeReservation (req, res, next, body, username) {
-  
   Default.makeReservation(body, username)
     .then(function (response) {
       utils.writeJson(res, response, response.code);
@@ -125,6 +175,7 @@ module.exports.makeReservation = function makeReservation (req, res, next, body,
     });
 };
 
+// Function to update exercise progress for a specific day and exercise
 module.exports.updateExerciseProgress = function updateExerciseProgress (req, res, next, day, name, weight, reps, username) {
   Default.updateExerciseProgress(day, name, weight, reps, username)
     .then(function (response) {
@@ -135,6 +186,7 @@ module.exports.updateExerciseProgress = function updateExerciseProgress (req, re
     });
 };
 
+// Function to update personal information for the user
 module.exports.updatePersonalInfo = function updatePersonalInfo (req, res, next, body, username) {
   Default.updatePersonalInfo(body, username)
     .then(function (response) {
