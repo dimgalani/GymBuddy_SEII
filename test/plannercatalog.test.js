b6478b41dbb9a4b34fe7fe8ebdd1d728d1c46fa0
Afroditi Avramidou <153501250+AfroditiAvr@users.noreply.github.com>
2025-01-05 20:29:53 +0200

    Update plannercatalog.test.js

--- test/plannercatalog.test.js
+++ test/plannercatalog.test.js
@@ -22,78 +22,7 @@ test.after((t) => {
  * edge cases, such as invalid requests or duplicate data.
  */
 
-// GET /planner //
-test("GET /user/{username}/planner with Bad Request (no day parameter)", async (t) => {
-	const { body, statusCode } = await t.context.got("user/default/planner", {
-		throwHttpErrors: false,
-	});
-	t.is(statusCode, 400);   
-});
 
-
-test("GET /user/{username}/planner with Bad Request or Not Found", async (t) => {
-    //try {
-        const { body, statusCode } = await t.context.got("user/john_doe/planner", {
-            searchParams: {
-                day: 5,
-            },
-            throwHttpErrors: false,
-        });
-		t.is(statusCode, 404);
-});
-
-
-test("GET /user/{usename}/planner with Bad Request (wrong day datatype)", async (t) => {
-	const { body, statusCode } = await t.context.got("user/default/planner", {
-		searchParams: {
-			day: "invalid_day"
-		},
-		throwHttpErrors: false
-	});
-	t.is(statusCode, 400);
-});
-
-
-test("GET /user/{usename}/planner with Correct Request", async (t) => {
-		const { body, statusCode } = await t.context.got("user/john_doe/planner", {
-			searchParams: {
-				day: 1
-			},
-			throwHttpErrors: false
-		});
-		t.is(statusCode, 200);
-		t.deepEqual(body, {
-			currentDate: 1,
-			exercisesList: [
-			{
-				name: "Romanian Deadlift",
-				notes: "Focus on form",
-				weightPerDateEntries: [60, 65],
-				repetitionsPerDateEntries: [8, 12],
-			},
-			{
-				name: "Hip Thrust",
-				notes: "Keep back straight",
-				weightPerDateEntries: [80, 85],
-				repetitionsPerDateEntries: [10, 15],
-			},
-			],
-		});
-});
-
-test("GET /user/{username}/planner with Default User", async (t) => {
-	const { body, statusCode } = await t.context.got("user/default/planner", {
-		searchParams: {
-		day: 1,
-		},
-	});
-	
-	t.is(statusCode, 200);
-	t.deepEqual(body, {
-		currentDate: 1,
-		exercisesList: [],
-	});
-});
 
 
  // GET /planner/catalog //
