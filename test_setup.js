206d8048545142078dc7f0698008b1048a5afc88
Emily Palaska <aimilia.p2@gmail.com>
2025-01-06 14:55:46 +0200

    add comments

--- test_setup.js
+++ test_setup.js
@@ -1,9 +1,11 @@
 'use strict';
 
+// Import necessary libraries
 var path = require('path');
 var http = require('http');
 var oas3Tools = require('oas3-tools');
-var serverPort = process.env.PORT || 8080;  // Default to 8080 if no PORT is specified
+// Default to 8080 if no PORT is specified
+var serverPort = process.env.PORT || 8080;  
 const got = require('got');
 
 // swaggerRouter configuration
@@ -13,9 +15,11 @@ var options = {
     },
 };
 
+// app initialization through oas3Tools
 var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
 var app = expressAppConfig.getApp();
 
+// Function that sets up the server to a n unused port, used by tests
 async function setupTestContext(t, app) {
     t.context.server = http.createServer(app);
     const server = t.context.server.listen();
@@ -26,6 +30,7 @@ async function setupTestContext(t, app) {
     });
 }
 
+// Functions to close the server after the tests have finished running
 function teardownTestContext(t) {
     t.context.server.close();
 }
