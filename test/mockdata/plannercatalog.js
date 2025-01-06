92556efa35e8c6a86af6d247aaaf8afd1d9ac7d8
Emily Palaska <aimilia.p2@gmail.com>
2025-01-06 13:49:41 +0200

    more breaking down

--- test/mockdata/plannercatalog.js
+++ test/mockdata/plannercatalog.js
@@ -1,30 +1,6 @@
 // Default notes from different file
 const { DEFAULT_NOTES } = require("./defaultnotes")
-
-// Data for the default user exercise and planner tests
-const defaultUser = {
-    latPullDown: {
-        name: "Lat Pull Down",
-            notes: DEFAULT_NOTES.latPullDown,
-            repetitionsPerDateEntries: [8, 10],
-            weightPerDateEntries: [40, 40],
-    },
-    deadlift: {
-        name: "Deadlift",
-            notes: DEFAULT_NOTES.deadlift,
-            repetitionsPerDateEntries: [8, 10],
-            weightPerDateEntries: [45, 45],
-    },
-    new: {name: "Deadlift", notes: DEFAULT_NOTES.deadlift,},
-    benchPress: {
-        name: "Bench Press",
-        notes: DEFAULT_NOTES.benchPress,
-    },
-    planner: {
-        currentDate: 1,
-        exercisesList: [],
-    }
-}
+const { defaultUser } = require("./defaultuser")
 
 // Data used for john_doe user exercise tests
 const johnDoeLatPulldown = {
