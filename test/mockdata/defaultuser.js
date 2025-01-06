2c8c75fb1c04bca77a136bf3962b176381780f49
Emily Palaska <aimilia.p2@gmail.com>
2025-01-06 15:02:57 +0200

    Update defaultuser.js

--- test/mockdata/defaultuser.js
+++ test/mockdata/defaultuser.js
@@ -1,19 +1,28 @@
+/*
+This files contains the mock data regarding the planner catalog tests
+It is being imported in plannercatalog.js
+Tests are located in plannercatalog.test.js
+*/
+
 // Default notes from different file
 const { DEFAULT_NOTES } = require("./defaultnotes")
 
 // used by "GET /user/{usename}/planner/catalog returns correct response and status code"
+const repetitionsPerDateEntries = [8,10]
+var weightPerDateEntries = [40,40]
 const latPullDown = {
     name: "Lat Pull Down",
         notes: DEFAULT_NOTES.latPullDown,
-        repetitionsPerDateEntries: [8, 10],
-        weightPerDateEntries: [40, 40],
+        repetitionsPerDateEntries: repetitionsPerDateEntries,
+        weightPerDateEntries: weightPerDateEntries,
 }
 // used by "POST /user/{username}/planner/catalog with Bad Request - Already existing exercise"
+var weightPerDateEntries = [45,45]
 const deadlift = {
     name: "Deadlift",
-        notes: DEFAULT_NOTES.deadlift,
-        repetitionsPerDateEntries: [8, 10],
-        weightPerDateEntries: [45, 45],
+    notes: DEFAULT_NOTES.deadlift,
+    repetitionsPerDateEntries: repetitionsPerDateEntries,
+    weightPerDateEntries: weightPerDateEntries,
 }
 // used by "POST /user/{username}/planner/catalog with Bad Request - Already existing exercise"
 const benchPress = {
