009ade035df79aab3a150c356ccf6c23af02c44b
Emily Palaska <aimilia.p2@gmail.com>
2025-01-06 15:35:37 +0200

    break down data

--- test/mockdata/exercisedetails.js
+++ test/mockdata/exercisedetails.js
@@ -3,36 +3,32 @@ This file contains the exercise details used
 for the plannerprogress.test.js test file
 it is divided into three parts for maintainability purposes
 */
+
+const {weightPerDateEntries, repetitionsPerDateEntries} = require('./weightreps')
 // used by "GET /user/{username}/planner/progress returns exercise details successfully"
 
 // first mock exercise
-var weightPerDateEntries = [5, 6, 6, 8, 8, 5, 6, 6, 8, 8]
-var repetitionsPerDateEntries = [ 10, 10, 15, 10, 10 ]
 const exercise1 = {
     notes : "note1",
     name : "exercise_1",
-    weightPerDateEntries : weightPerDateEntries,
-    repetitionsPerDateEntries : repetitionsPerDateEntries
+    weightPerDateEntries : weightPerDateEntries[0],
+    repetitionsPerDateEntries : repetitionsPerDateEntries[0]
 }
 
 // second mock exercise
-var weightPerDateEntries = [ 20, 25, 25, 25 ,30, 20, 25, 25, 25 ,30]
-var repetitionsPerDateEntries = [ 15, 15, 15, 20, 15 ]
 const exercise2 = {
     notes : "note2",
     name : "exercise_2",
-    weightPerDateEntries : weightPerDateEntries,
-    repetitionsPerDateEntries : repetitionsPerDateEntries
+    weightPerDateEntries : weightPerDateEntries[1],
+    repetitionsPerDateEntries : repetitionsPerDateEntries[1]
 }
 
 // third mock exercise
-var weightPerDateEntries = [ 30, 35, 35, 40, 45, 30, 35, 35, 40, 45]
-var repetitionsPerDateEntries = [ 5, 5, 5, 5 ,8 ]
 const exercise3 = {
     notes : "note3",
     name : "exercise_3",
-    weightPerDateEntries : weightPerDateEntries,
-    repetitionsPerDateEntries : repetitionsPerDateEntries
+    weightPerDateEntries : weightPerDateEntries[2],
+    repetitionsPerDateEntries : repetitionsPerDateEntries[2]
 }
 
 module.exports = {exercise1, exercise2, exercise3}
